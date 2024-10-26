6a565180df68a30b6fd69d86f681520e56a6c6a8
Abhishek Kumar <avi.kr16@gmail.com>
2024-10-26 14:18:35 +0530

    veritical bouncing of the ball added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:brick_breaker/ball.dart';
 import 'package:brick_breaker/coverscreen.dart';
+import 'package:brick_breaker/player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,10 +13,19 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+enum direction { UP, DOWN }
+
 class _HomePageState extends State<HomePage> {
   // Ball variable
   double ballX = 0;
   double ballY = 0;
+  var ballDirection = direction.DOWN;
+
+  // var ballDirection = direction.DOWN;
+
+  // Player variable
+  double playerX = 0;
+  double playerWidth = 0.3; // out of 3
 
   // Game Settings
   bool hasGameStarted = false;
@@ -22,27 +33,85 @@ class _HomePageState extends State<HomePage> {
   void startGame() {
     hasGameStarted = true;
     Timer.periodic(Duration(milliseconds: 10), (timer) {
-      setState(() {
-        ballY -= 0.01;
-      });
+      // update direction
+      updateDirection();
+      // move ball
+      moveBall();
     });
+  }
+
+  void moveBall() {
+    setState(() {
+      if (ballDirection == direction.DOWN) {
+        ballY += 0.01;
+      } else if (ballDirection == direction.UP) {
+        ballY -= 0.01;
+      }
+    });
+  }
+
+  void updateDirection() {
+    setState(() {
+      if (ballY >= 0.9) {
+        ballDirection = direction.UP;
+      } else if (ballY <= -0.9) {
+        ballDirection = direction.DOWN;
+      }
+    });
+  }
+
+  // Move player left
+  void moveLeft() {
+    // only move left if moving left does not move player off the screen
+    if (!(playerX - 0.2 < -1)) {
+      setState(() {
+        playerX -= 0.2;
+      });
+    }
+  }
+
+  // Move Player right
+  void moveRight() {
+    // only move right if moving right does not move player off the screen
+    if (!(playerX + playerWidth >= 1)) {
+      // setState(() {
+      playerX += 0.2;
+      // });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: startGame,
-      child: Scaffold(
-        backgroundColor: Colors.purple[100],
-        body: Center(
-          child: Stack(
-            children: [
-              // tap to play
-              Coverscreen(hasGameStarted: hasGameStarted),
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (event) {
+        if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+          moveLeft();
+        } else if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+          moveRight();
+        }
+      },
+      child: GestureDetector(
+        onTap: startGame,
+        child: Scaffold(
+          backgroundColor: Colors.purple[100],
+          body: Center(
+            child: Stack(
+              children: [
+                // tap to play
+                Coverscreen(hasGameStarted: hasGameStarted),
 
-              // Ball
-              MyBall(ballX: ballX, ballY: ballY)
-            ],
+                // Ball
+                MyBall(ballX: ballX, ballY: ballY),
+
+                // Player
+                MyPlayer(
+                  playerX: playerX,
+                  playerWidth: playerWidth,
+                )
+              ],
+            ),
           ),
         ),
       ),
